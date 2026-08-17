83a53e416f9b32debd4fea539e2d67fc1b933fe9
ramshyam11995-sys <ramshyam11995@gmail.com>
2026-08-18 01:09:40 +0530

    Add alt text to company logos in Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -84,11 +84,11 @@ const Home = () => {
       </form>
 
       <div className="flex flex-wrap items-center justify-center gap-16 md:gap-20 mx-auto mt-16">
-        <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/framer.svg" alt="" />
-        <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/huawei.svg" alt="" />
-        <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/instagram.svg" alt="" />
-        <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/microsoft.svg" alt="" />
-        <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/walmart.svg" alt="" />
+        <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/framer.svg" alt="Framer" />
+        <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/huawei.svg" alt="Huawei" />
+        <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/instagram.svg" alt="Instagram" />
+        <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/microsoft.svg" alt="Microsoft" />
+        <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/walmart.svg" alt="Walmart" />
       </div>
     </section>
   );
