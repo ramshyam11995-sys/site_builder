f7ad7cdc0ff4a40075e654e8b094ce1f4e0fb809
ramshyam11995-sys <ramshyam11995@gmail.com>
2026-08-19 13:01:50 +0530

    Update Huawei logo image source
    Replaced Huawei logo image source with a local file.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -86,7 +86,9 @@ const Home = () => {
     
       <div className="flex flex-wrap items-center justify-center gap-16 md:gap-20 mx-auto mt-16">
         <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/framer.svg" alt="Framer" />
-        <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/huawei.svg" alt="Huawei" />
+        <img src="/public/huawei.svg" alt="Huawei Logo" />
+
+        {/* <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/huawei.svg" alt="Huawei" /> */}
         <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/instagram.svg" alt="Instagram" />
         <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/microsoft.svg" alt="Microsoft" />
         <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/walmart.svg" alt="Walmart" />
