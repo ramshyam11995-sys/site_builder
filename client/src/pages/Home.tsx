df2c24f5ca90e0925f47d3aaa64a084f3e0f784b
ramshyam11995-sys <ramshyam11995@gmail.com>
2026-08-19 09:55:57 +0530

    Add error handling for Framer image
    Added onError handler to Framer image for error logging.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -85,7 +85,7 @@ const Home = () => {
       </form>
 
       <div className="flex flex-wrap items-center justify-center gap-16 md:gap-20 mx-auto mt-16">
-        <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/framer.svg" alt="Framer" />
+        <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/framer.svg" alt="Framer" onError={(e) => console.log("FRAMER IMAGE FAILED", e.currentTarget.src)}/>
         <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/huawei.svg" alt="Huawei" />
         <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/instagram.svg" alt="Instagram" />
         <img className="max-w-28 md:max-w-32" src="https://saasly.prebuiltui.com/assets/companies-logo/microsoft.svg" alt="Microsoft" />
